cf52911f9cc2a0fc78c4d792d18a27c0f349c48e
TeslaC00 <111948419+TeslaC00@users.noreply.github.com>
2023-04-23 19:42:08 +0530

    commit

--- GUI Practice/src/Frame.java
+++ GUI Practice/src/Frame.java
@@ -1,5 +1,14 @@
+import java.awt.Color;
 import javax.swing.JFrame;
 
 public class Frame extends JFrame{
     
+    Frame(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500, 500);
+        this.setTitle("GUI Window");
+        this.setLocationRelativeTo(null);
+        this.getContentPane().setBackground(Color.BLACK);
+        this.setVisible(true);
+    }
 }
