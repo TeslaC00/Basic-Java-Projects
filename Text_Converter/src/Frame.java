1a60e4512f50fc89e9bef72022cd69593916d563
TeslaC00 <111948419+TeslaC00@users.noreply.github.com>
2023-04-24 19:41:03 +0530

    commit

--- Text_Converter/src/Frame.java
+++ Text_Converter/src/Frame.java
@@ -1,13 +1,19 @@
+import javax.swing.JColorChooser;
+import javax.swing.JComboBox;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.ScrollPaneConstants;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GraphicsEnvironment;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -24,45 +30,72 @@ public class Frame extends JFrame implements ActionListener {
     private JMenuItem load;
     private JMenuItem save;
     private JMenuItem exit;
+    private JMenu color;
+    private JMenuItem fontColor;
+    private JMenuItem bgColor;
     private String words[][];
     private JFileChooser fileChooser;
+    private JScrollPane scrollPane;
+    private JComboBox fontBox;
+    private Font defaultFont;
 
     Frame(){
 
+        //---------<<menu-bar>>----------//
         menuBar = new JMenuBar();
         fileChooser = new JFileChooser(".");
-        textArea = new JTextArea("\n\n\n\n\t\tThis Is A Text Area");
+        textArea = new JTextArea("\n\t\tThis Is A Text Area");
         fileMenu = new JMenu("File");
+        color = new JMenu("Color");
         open = new JMenuItem("Open");
         load = new JMenuItem("Load");
         save = new JMenuItem("Save");
         exit = new JMenuItem("Exit");
+        fontColor = new JMenuItem("Font Color");
+        bgColor = new JMenuItem("Text Area Color");
 
         load.addActionListener(this);
         save.addActionListener(this);
         exit.addActionListener(this);
         open.addActionListener(this);
+        fontColor.addActionListener(this);
+        bgColor.addActionListener(this);
 
+        color.add(fontColor);
+        color.add(bgColor);
         fileMenu.add(open);
         fileMenu.add(load);
         fileMenu.add(save);
         fileMenu.add(exit);
         menuBar.add(fileMenu);
+        menuBar.add(color);
+        //---------<<menu-bar>>----------//
 
+        defaultFont = new Font("Ink Free", Font.PLAIN, 20);
         textArea.setEditable(false);
         textArea.setLineWrap(true);
+        textArea.setFont(defaultFont);
         textArea.setWrapStyleWord(true);
-        textArea.setPreferredSize(new Dimension(420, 420));
         textArea.setBackground(Color.cyan);
 
+        scrollPane = new JScrollPane(textArea);
+        scrollPane.setPreferredSize(new Dimension(670, 610));
+        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED);
+
+        String fonts[] = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+        fontBox = new JComboBox<>(fonts);
+        fontBox.setSelectedItem("Ink Free");
+        fontBox.addActionListener(this);
+
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(500, 500);
+        this.setSize(720, 720);
         this.setLocationRelativeTo(null);
         this.setResizable(false);
         this.getContentPane().setBackground(Color.DARK_GRAY);
         this.setLayout(new FlowLayout());
         this.setJMenuBar(menuBar);
-        this.add(textArea);
+        this.add(fontBox);
+        this.add(scrollPane);
         this.setVisible(true);
     }
 
@@ -95,9 +128,9 @@ public class Frame extends JFrame implements ActionListener {
         }
 
         if(e.getSource()==open){
-            if(textArea.getText()!=null) textArea.setText("");
             int response = fileChooser.showOpenDialog(null);
             if(response==JFileChooser.APPROVE_OPTION){
+                if(textArea.getText()!=null) textArea.setText("");
                 File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
                 if(file.isFile()){
                     try (Scanner scan = new Scanner(file)) {
@@ -109,6 +142,20 @@ public class Frame extends JFrame implements ActionListener {
                     }
                 }
             }
+        }
+
+        if(e.getSource()==fontBox){
+            textArea.setFont(new Font(fontBox.getSelectedItem().toString(),Font.PLAIN,20));
+        }
+
+        if(e.getSource()==fontColor){
+            Color fontC = JColorChooser.showDialog(null, "Font Color Chooser", Color.black);
+            textArea.setForeground(fontC);
+        }
+
+        if(e.getSource()==bgColor){
+            Color bgC = JColorChooser.showDialog(null, "Text Area Color Chooser", Color.cyan);
+            textArea.setBackground(bgC);
         }
     }
     
