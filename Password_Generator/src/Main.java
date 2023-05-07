2eb5a3de7623e95e68c3f8bd938f9d6e66e9fc0d
TeslaC00 <111948419+TeslaC00@users.noreply.github.com>
2023-05-07 13:51:35 +0530

    changed main to menu and app to menu

--- Password_Generator/src/Main.java
+++ Password_Generator/src/Main.java
@@ -1,40 +1,7 @@
 public class Main {
+    
+    public static void main(String[] args) {
 
-    private int choice;
-
-    public Main(){
-
-        showMainMenu();   //shows main screen text
-        takeUserChoice();   //takes the user choice
-    }
-
-    private void showMainMenu() {
-
-        System.out.println("\033[H\033[2J");    //clears the screen
-        System.out.println("Welcome to Password Generator and Strength Checker");
-        System.out.println("1.Generate a new Password");
-        System.out.println("2.Check Strength of a Password");
-        System.out.println("0.Exit");
-        System.out.println("What do you want to do?\nPlease enter your choice");
-    }
-
-    private void takeUserChoice(){
-        
-        do {
-            choice = Helper.promptInput();
-            switch(choice){
-                case 0:
-                    System.exit(0);
-                    break;
-                case 1:
-                    new PassBuilder();
-                    break;
-                case 2:
-                    new StrengthChecker();
-                    break;
-                default: 
-                    System.err.println("Give valid number as input");
-            }
-        } while(!Helper.isValidInteger(choice, 0, 2));
+        new Menu();
     }
 }
---
+++ Password_Generator/src/Menu.java
@@ -0,0 +1,66 @@
+public class Menu {
+
+    private int choice;
+    private PasswordBuilder passwordBuilder;
+
+    public Menu(){
+
+        passwordBuilder = new PasswordBuilder();
+        showMainMenu();   //shows main screen text
+    }
+    
+    private void showMainMenu() {
+        
+        System.out.println("\033[H\033[2J");    //clears the screen
+        System.out.println("Welcome to Password Generator and Strength Checker");
+        System.out.println("1.Generate a new Password");
+        System.out.println("2.Check Strength of a Password");
+        System.out.println("0.Exit");
+        System.out.println("What do you want to do?\nPlease enter your choice");
+        takeUserChoice();   //takes the user choice
+    }
+
+    private void takeUserChoice(){
+        
+        do {
+            choice = Helper.promptInput();
+            switch(choice){
+                case 0:
+                    System.exit(0);
+                    break;
+                case 1:
+                    passwordBuilderMenu();
+                    break;
+                case 2:
+                    new StrengthChecker();
+                    break;
+                default: 
+                    System.err.println("Give valid number as input");
+            }
+        } while(!Helper.isValidInteger(choice, 0, 2));
+    }
+
+    private void passwordBuilderMenu(){
+
+        System.out.println("\033[H\033[2J");    //clears the screen
+        System.out.println("Welcome to Password Builder\n");
+        System.out.println("0.Go back\n1.Set Password Parameters\n2.Generate Password");
+        System.out.print("Please give your choice: ");
+        
+        do {
+            choice = Helper.promptInput();
+            switch(choice){
+                case 0:
+                    showMainMenu();
+                    break;
+                case 1: 
+                    passwordBuilder.setPassParameters();
+                case 2: 
+                    passwordBuilder.generate();
+                    break;
+                default:
+                    System.err.println("Please give a valid number");
+            }
+        } while(!Helper.isValidInteger(choice, 0, 2));
+    }
+}
