a7e4da9da82528005882d8072933aaeb71508d83
TeslaC00 <111948419+TeslaC00@users.noreply.github.com>
2023-05-16 23:08:51 +0530

    added file handling

--- Password_Generator/src/Display.java
+++ Password_Generator/src/Display.java
@@ -1,6 +1,11 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Display {
 
-    String passwordA, passwordB, passwordC;
+    private String passwordA, passwordB, passwordC;
 
     Display(String A, String B, String C) {
 
@@ -19,13 +24,39 @@ public class Display {
     }
 
     public void save() {
+        File allPassword = new File("All_Password.txt");
+        Boolean newFile = false;
+        try {
+            newFile = allPassword.createNewFile();
+        } catch (IOException e) {
+            System.err.println("Error in creating new file");
+        }
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(allPassword, true))) {
+            if (newFile)
+                writer.append("All Generated Passwords: \n");
+            writer.append(passwordA + "\n");
+            writer.append(passwordB + "\n");
+            writer.append(passwordC + "\n");
+        } catch (IOException e1) {
+            System.err.println("File writer error");
+        }
     }
 
-}
-
-class Saver {
-
-    public static void userSave(String password) {
-        System.out.println("saved " + password);
+    public static void save(String password) {
+        File userPassword = new File("User_Password.txt");
+        Boolean newFile = false;
+        try {
+            newFile = userPassword.createNewFile();
+        } catch (IOException e) {
+            System.err.println("Error in creating new file");
+        }
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(userPassword, true))) {
+            if (newFile)
+                writer.append("User Passwords: \n");
+            writer.append(password + "\n");
+        } catch (IOException e1) {
+            System.err.println("File writer error");
+        }
     }
+
 }
